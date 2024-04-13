3b483a3032a659df97a060ee43cd96c1abb07419
nish <144939153+nishj347@users.noreply.github.com>
2024-04-14 00:28:35 +0530

    day15

---
+++ cpp/array and pointer.cpp
@@ -0,0 +1,37 @@
+#Author : nishj347 @github
+#include <iostream>
+using namespace std;
+
+class shop{
+float id;
+float price;
+public:
+void setdata(float id,float price){
+  this->id=id;
+  this->price=price;
+}
+void getdata(){
+  cout<<"the id of this item is "<<id<<endl;
+  cout<<"the price of this item:"<<price<<endl;
+}
+};
+
+int main() {
+  float id;
+  float price;
+  int size=3;
+  shop *ptr=new shop[size];
+  shop *ptrtemp=ptr;
+  for(int i=0;i<size;i++){
+  cout<<"enter the id of item"<<i+1<<endl;
+    cin>>id;
+    cin>>price;
+    ptr->setdata(id,price);
+    ptr++;
+  }
+  for(int i=0;i<size;i++){
+    ptrtemp->getdata();
+    ptrtemp++;
+  }
+  return 0;
+}
