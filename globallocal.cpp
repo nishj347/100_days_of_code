1111d38f05d5512bdd69d1ff5f85d8fd13b77181
nish <144939153+nishj347@users.noreply.github.com>
2024-06-01 17:27:57 +0530

    day24

---
+++ globallocal.cpp
@@ -0,0 +1,28 @@
+// Author: nishj347 @github
+// Local and Global Class: Implement a local class within a function and a global class in
+// the same program and demonstrate their usage.
+#include <iostream>
+using namespace std;
+class global{
+    public:
+    void display(){
+        cout<<"a global class"<<endl;
+    }
+};
+void localfunction(){
+    class local{
+        public:
+        void display(){
+        cout<<"a local class"<<endl;
+    }
+};
+local obj;
+obj.display();
+}
+
+int main(){
+    global g;
+    g.display();
+    localfunction();
+    return 0;
+}
